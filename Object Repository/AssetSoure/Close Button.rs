16284c85e3cfe6f06638dfc27f41af714d8bd1db
Shirisha271 <Shirisha271@users.noreply.github.com>
2021-01-07 14:13:53 +0530

    AssetSource

---
+++ Object Repository/AssetSoure/Close Button.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Close Button</name>
+   <tag></tag>
+   <elementGuidId>a8e7f8eb-d840-4161-a6c3-fab1c77cca22</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@class=&quot;btn btn-secondary&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//button[@class=&quot;btn btn-secondary&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/AssetSoure/Tenant.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Tenant</name>
+   <tag></tag>
+   <elementGuidId>5b4102fc-19bf-40df-b100-56e2bb6074e1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//label[text()='Tenant']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//label[text()='Tenant']</value>
+   </webElementProperties>
+</WebElementEntity>
