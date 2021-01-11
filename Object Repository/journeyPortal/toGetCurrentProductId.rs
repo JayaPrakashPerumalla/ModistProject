2762f627e329b32a532567d02515b02f71c3d1c7
JayaPrakashPerumalla <76806778+JayaPrakashPerumalla@users.noreply.github.com>
2021-01-11 11:35:15 -0500

    left and right carot

---
+++ Object Repository/journeyPortal/toGetCurrentProductId.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>toGetCurrentProductId</name>
+   <tag></tag>
+   <elementGuidId>bb055404-79e1-4c7e-b98b-7eea48633e7b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='swiper-slide swiper-slide-active']//div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='swiper-slide swiper-slide-active']//div</value>
+   </webElementProperties>
+</WebElementEntity>
