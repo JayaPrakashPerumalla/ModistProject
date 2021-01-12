2f07733a3f6664eac3955191ab84e847f077ecee
JayaPrakashPerumalla <76806778+JayaPrakashPerumalla@users.noreply.github.com>
2021-01-12 03:59:50 -0500

    executed

--- Scripts/Admin/Journey/createAccessCodeForJourneyAndVerifyIsTheCodeSuccessfullyCreated/Script1609925553195.groovy
+++ Scripts/Admin/Journey/createAccessCodeForJourneyAndVerifyIsTheCodeSuccessfullyCreated/Script1609925553195.groovy
@@ -15,6 +15,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+CustomKeywords.'pages.Journey.clickOnJourneyTab'()
+
 String journeyName = CustomKeywords.'pages.Journey.getRandomJourneyName'()
 
 String accessCode = CustomKeywords.'pages.Journey.getRandomAccessCode'()
